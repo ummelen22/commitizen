ea7cf90e06ef8ba1aff4f00cc8245b7818b5717e
ummelen22 <62067968+ummelen22@users.noreply.github.com>
2020-10-12 15:38:30 +0200

    Update check.py

--- commitizen/commands/check.py
+++ commitizen/commands/check.py
@@ -26,7 +26,8 @@ class Check:
         self.commit_msg_file: Optional[str] = arguments.get("commit_msg_file")
         self.commit_msg: Optional[str] = arguments.get("message")
         self.rev_range: Optional[str] = arguments.get("rev_range")
-
+        
+        print(self.commit_msg_file)
         self._valid_command_argument()
 
         self.config: BaseConfig = config
